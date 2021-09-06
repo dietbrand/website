bd5b6a467e8acb0d4e76c6b2fc82190f9b098db2
Dietbrand Tollenaere <dietbrand.tollenaere@optimile.eu>
2021-09-06 16:54:48 +0200

    adds a clock

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+console.log("Hello! Lovely to see you here 😊.")
+
+const myVar = setInterval(myTimer, 1000);
+
+function myTimer() {
+    let d = new Date();
+    document.getElementById("displayClock").innerHTML = d.toLocaleTimeString();
+}
